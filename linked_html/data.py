bd16a70db4c1a258ad27c34567b00acef5865762
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-09 23:04:50 -0500

    test commit

--- linked_html/data.py
+++ linked_html/data.py
@@ -6,6 +6,6 @@ df = pd.read_csv('../Resources/cities.csv')
 # Save to file
 df.to_html('data.html', index=False)
 
-# Assign to string
+# Assign to string using to_html function
 table = df.to_html()
 print(table)
